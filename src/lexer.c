46773c63978b0825d97377ed9fdf32e34f89b843
abarriga <abarriga4242@gmail.com>
2023-04-11 16:54:20 +0200

    todo mergeado correctamente

--- src/lexer.c
+++ src/lexer.c
@@ -1,46 +1,5 @@
 #include "../includes/minishell.h"
 
-/**
- * @brief Esta funcion mira la entrada y verifica que haya si hay comillas o
- *  comilla sea en una cantidad par
- * 
- * @param input 
- * @return int 
- */
-// int	check_input_quotes(char *input)
-// {
-// 	t_quote	q;
-
-// 	while (input[q.i])
-// 	{
-// 		if (input[q.i] == '\"')
-// 		{
-// 			if (q.flag_quote == 1)
-// 				return (1);
-// 			q.quotes += 1;
-// 			if (q.flag_quotes == 0)
-// 				q.flag_quotes = 1;
-// 			else if (q.flag_quotes == 1)
-// 				q.flag_quotes = 0;
-// 		}
-// 		if (q.input[q.i] == '\'')
-// 		{
-// 			if (q.flag_quotes == 1)
-// 				return (1);
-// 			q.quote += 1;
-// 			if (q.flag_quote == 0)
-// 				q.flag_quote = 1;
-// 			else if (flag_quote == 1)
-// 				q.flag_quote = 0;
-// 		}
-// 		q.i += 1;
-// 	}
-// 	if (q.quotes % 2 == 0 && q.quote % 2 == 0)
-// 		return (0);
-// 	else
-// 		return (1);
-// }
-
 int	check_input_quotes(char *input)
 {
 	char	quote;
@@ -59,14 +18,6 @@ int	check_input_quotes(char *input)
 	return (0);
 }
 
-// void	init_quote(t_quote q)
-// {
-// 	q.i = 0;
-// 	q.quotes = 0;
-// 	q.quote = 0;
-// 	q.flag_quotes = 0;
-// 	q.flag_quote = 0;
-// }
 /**
  * @brief Función que hace un split a la entrada
  * 
